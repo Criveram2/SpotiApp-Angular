322c3963ea542fafb73458ec3b3d1ae51da06d0b
Camilo Rivera <camilo.rivera@vasslatam.com>
2020-07-05 14:47:52 -0500

    se crea mensaje de error en home, se crea consumo de servicio post para generar Token

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,19 +3,27 @@ import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
 })
-export class HomeComponent  {
+export class HomeComponent {
+  nuevasCanciones: any[] = [];
+  loading: boolean;
+  error: boolean;
+  mensajeError: string;
 
-nuevasCanciones: any[] = [];
-loading: boolean;
-  constructor( private spotifyService: SpotifyService) {
+  constructor(private spotifyService: SpotifyService) {
     this.loading = true;
-    this.spotifyService.getNewReleases().subscribe( (data: any) =>
-    {
-         this.nuevasCanciones = data;
-         this.loading = false;
-    });
-   }
-
+    this.error = false;
+    this.spotifyService.getNewReleases().subscribe(
+      (data: any) => {
+        this.nuevasCanciones = data;
+        this.loading = false;
+      },
+      (errorServicio) => {
+        this.error = true;
+        this.loading = false;
+        this.mensajeError = errorServicio.error.error.message;
+      }
+    );
+  }
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -9,7 +9,8 @@ export class SpotifyService {
   constructor(private http: HttpClient) {}
 
   getQuery(query: string) {
-    const token = 'BQCnsZWEWU_6OSP3p0XSLdU2xbaZVQGfi-WPLEGJlGNLjVgy-W83VS0082wtlXGeYgltVb1mblNGhvts8OI';
+    const token =
+      'BQD2CSjI3mxv4E35Givc83NBxD39E1D_WIrMfXuv07EwUdYEzgHHrOtUjAn7xCZGsAUf7GDe3nebfvAqOtE';
     const url = `https://api.spotify.com/v1/${query}`;
 
     const headers = new HttpHeaders({
@@ -19,21 +20,57 @@ export class SpotifyService {
   }
 
   getNewReleases() {
-    return this.getQuery('browse/new-releases')
-    .pipe(map((data) => data['albums'].items));
+    return this.getQuery('browse/new-releases').pipe(
+      map((data) => data['albums'].items)
+    );
   }
 
   getArtistas(termino: string) {
-    return this.getQuery( `search?type=artist&limit=15&q=${termino}`)
-    .pipe(map((data) => data['artists'].items));
+    return this.getQuery(`search?type=artist&limit=15&q=${termino}`).pipe(
+      map((data) => data['artists'].items)
+    );
   }
 
   getArtistaByid(id: string) {
-    return this.getQuery( `artists/${id}`);
+    return this.getQuery(`artists/${id}`);
   }
 
   getTopTracksByIdArtista(id: string) {
-    return this.getQuery( `artists/${id}/top-tracks?country=es`)
-    .pipe(map((data) => data['tracks']));
+    return this.getQuery(`artists/${id}/top-tracks?country=es`).pipe(
+      map((data) => data['tracks'])
+    );
+  }
+
+
+  /*
+   * Consumo de servicio web para generar token de la aplicacion
+   * @author : Camilo Rivera
+   * @Since  : 05-07-2020
+   **/
+  private generarToken(): string {
+    let headersPeticion = new HttpHeaders();
+    headersPeticion = headersPeticion.set(
+      'Content-Type',
+      'application/x-www-form-urlencoded'
+    );
+    const parametros = new HttpParams()
+      .set('grant_type', 'client_credentials')
+      .set('client_id', 'a02c5601c9224629aa632c32f8b588ab')
+      .set('client_secret', 'be220004160f4e14bac2b673e24a5571');
+
+    const urlToken = 'https://accounts.spotify.com/api/token';
+    this.http
+      .post(urlToken, parametros, { headers: headersPeticion })
+      .subscribe(
+        (data: any) => {
+          console.log('respuesta :' + JSON.stringify(data));
+          return data.access_token;
+        },
+        (errorServicio) => {
+          console.log('Error: ' + JSON.stringify(errorServicio));
+          return '';
+        }
+      );
+    return '';
   }
 }
